71afa9abf86e939b07ce19247ec933e95f9f89be
Sharon Kim <sharonkim1709@gmail.com>
2020-08-04 20:31:05 -0400

    hard coded project img and added font awesome with gh icon

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,50 @@
-import React from 'react';
-
+import React from "react";
+import "./App.css";
 function App() {
   return (
     <>
-     <h1>This is the beginning of my portfolio page</h1>
+    {/* Main Section */}
+      <div className="row">
+        <div className="col s12 center-align">
+          <img className="circle" alt="my pic" src="../profile.png" />
+          <h1>Welcome to my portfolio page!</h1>
+          <p>Full-Stack Web Developer with skills in MERN and JS. </p>
+        </div>
+      </div>
+      {/* Projects Section */}
+      <div className="row center-align">
+        <h2>Projects</h2>
+        <div className="col s6">
+          <img
+            alt="burger"
+            className="responsive-img"
+            src="../spellbindr-screenshot.png"
+          />
+        </div>
+        <div className="col s6">
+          <img
+            alt="burger"
+            className="responsive-img"
+            src="../spellbindr-screenshot.png"
+          />
+        </div>
+        <div className="col s6">
+          <img
+            alt="burger"
+            className="responsive-img"
+            src="../spellbindr-screenshot.png"
+          />
+        </div>
+        <div className="col s6">
+          <img
+            alt="burger"
+            className="responsive-img"
+            src="../spellbindr-screenshot.png"
+          />
+        </div>
+      </div>
+      {/* Contact Section */}
+      <h1>Github<i class="fa fa-github" aria-hidden="true"></i></h1>
     </>
   );
 }
