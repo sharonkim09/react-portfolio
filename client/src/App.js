f62abd7d081abd76bd468fb38c516f11ed42a340
Sharon Kim <sharonkim1709@gmail.com>
2020-08-04 21:24:51 -0400

    moved header to seperate component

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,10 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header/Header"
 function App() {
   return (
     <>
-      <div className="navbar-fixed">
-        <nav className="black">
-          <div className="container">
-            <div className="nav-wrapper">
-              <a href="" className="brand-logo">
-                My Portfolio
-              </a>
-              <ul className="right hide-on-med-and-down" id="navList">
-                <li>
-                  <a href="">Home</a>
-                </li>
-                <li>
-                  <a href="">About Me</a>
-                </li>
-                <li>
-                  <a href="">Projects</a>
-                </li>
-                <li>
-                  <a href="">Contact</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-      </div>
+<Header/>
       {/* Main Section */}
       <div className="row">
         <div className="col s12 center-align">
