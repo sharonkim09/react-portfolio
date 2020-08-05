a3124df895dcfb499db2b62590767a5ac14d1efa
Sharon Kim <sharonkim1709@gmail.com>
2020-08-04 21:29:37 -0400

    fixed errors of className and href showing in console

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -6,21 +6,21 @@ const Header = () => {
         <nav className="black">
           <div className="container">
             <div className="nav-wrapper">
-              <a href="" className="brand-logo">
+              <a href="/" className="brand-logo">
                 My Portfolio
               </a>
               <ul className="right hide-on-med-and-down" id="navList">
                 <li>
-                  <a href="">Home</a>
+                  <a href="/">Home</a>
                 </li>
                 <li>
-                  <a href="">About Me</a>
+                  <a href="/">About Me</a>
                 </li>
                 <li>
-                  <a href="">Projects</a>
+                  <a href="/">Projects</a>
                 </li>
                 <li>
-                  <a href="">Contact</a>
+                  <a href="/">Contact</a>
                 </li>
               </ul>
             </div>
