33f4093b87dba591edfe07322c51a91620b2e696
Sharon Kim <sharonkim1709@gmail.com>
2020-08-05 22:44:36 -0400

    moved social media icons to footer and linked successfully

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -5,7 +5,28 @@ const Footer = () => {
     <footer className="black page-footer">
       <div className="footer-copyright center">
         <div className="container">
-          © 2020 Copyright Text
+          <div className="row">
+            <div className="col s3 l3">
+              <p>
+              <a target="_blank" href="https://github.com/sharonkim09"><i className="fa fa-github" aria-hidden="true"></i></a>
+              </p>
+            </div>
+            <div className="col s3 l3">
+              <p>
+                <i className="fa fa-envelope" aria-hidden="true"></i>
+              </p>
+            </div>
+            <div className="col s3 l3">
+              <p>
+              <a target="_blank" href="https://www.linkedin.com/in/sharon-kim-7a2766196/"><i className="fa fa-linkedin" aria-hidden="true"></i></a>
+              </p>
+            </div>
+            <div className="col s3 l3">
+              <p>
+              <a target="_blank" href="https://drive.google.com/file/d/13QHCUgUyTN4OYuOiC9SV-DBYF3JSm6yh/view?usp=sharing"><i className="fa fa-address-card" aria-hidden="true"></i></a>
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </footer>
