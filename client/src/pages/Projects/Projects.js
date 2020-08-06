54f7ab9c713bb326fb760aa025daf320dca05810
Sharon Kim <sharonkim1709@gmail.com>
2020-08-05 23:54:03 -0400

    styled projects into card and added buttons to app pages

--- client/src/pages/Projects/Projects.js
+++ client/src/pages/Projects/Projects.js
@@ -1,71 +1,178 @@
-import React from 'react';
+import React from "react";
 
 const ProjectSection = () => {
-    return (
-        <div className="row center-align">
-        <h2>Projects</h2>
-        <div className="col s6">
-          <img
-            alt="burger"
-            className="responsive-img"
-            src="../spellbindr-screenshot.png"
-          />
-           <h3>Spellbindr</h3>
-         <p><a href="https://arhamilton92.github.io/spellbindr/">GitHub Repo Link</a></p>
-          <a target="_blank" href="https://github.com/arhamilton92/spellbindr">Deployed Website Link</a>
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col s12 m6">
+          <div className="card">
+            <div className="card-image">
+              <img alt="random wizard generated" src="../spellbindr-screenshot.png" />
+              <span className="card-title">Spellbindr</span>
+              <a
+                href="https://arhamilton92.github.io/spellbindr/"
+                className="btn-floating halfway-fab waves-effect waves-light red"
+              >
+                <i className="fa fa-github"></i>
+              </a>
+              <a
+                href="https://github.com/arhamilton92/spellbindr"
+                className="btn-floating left halfway-fab waves-effect waves-light red"
+              >
+                <i className="fa fa-window-maximize"></i>
+              </a>
+            </div>
+            <div className="card-content">
+              <p>
+                I am a very simple card. I am good at containing small bits of
+                information. I am convenient because I require little markup to
+                use effectively.
+              </p>
+            </div>
+          </div>
         </div>
-        <div className="col s6">
-          <img
-            alt="password"
-            className="responsive-img"
-            src="../password-generator.png"
-          />
-           <h3>Password Generator</h3>
-          <p><a href="https://github.com/sharonkim09/gt-hw-js-password-generator">GitHub Repo Link</a></p>
-          <p><a href="https://sharonkim09.github.io/gt-hw-js-password-generator">Deployed Website Link</a></p>
-        </div>
-        <div className="col s6">
-          <img
-            alt="burger"
-            className="responsive-img"
-            src="../burger-screenshot.png"
-          />
-           <h3>Burger</h3>
-          <p><a href="https://github.com/sharonkim09/burger">GitHub Repo Link</a></p>
-          <p><a href="https://damp-depths-84040.herokuapp.com/">Deployed Website Link</a></p>
-        </div>
-        <div className="col s6">
-          <img
-            alt="burger"
-            className="responsive-img"
-            src="../donendusted.png"
-          />
-           <h3>Done N Dusted</h3>
-          <p><a href="https://github.com/yoohooitstoo/grocr-receipt-app">GitHub Repo Link</a></p>
-          <p><a href="https://fathomless-sands-26873.herokuapp.com/">Deployed Website Link</a></p>
-        </div>
-        <div className="col s6">
-          <img
-            alt="burger"
-            className="responsive-img"
-            src="../note-screenshot.png"
-          />
-           <h3>Note Taker</h3>
-          <p><a href="https://github.com/sharonkim09/gt-hw-note-taker-2">GitHub Repo Link</a></p>
-          <p><a href="https://peaceful-inlet-73696.herokuapp.com/">Deployed Website Link</a></p>
-        </div>
-        <div className="col s6">
-          <img
-            alt="burger"
-            className="responsive-img"
-            src="../note-screenshot.png"
-          />
-           <h3>React Employee App</h3>
-          <p><a target="_blank" href="https://github.com/sharonkim09/react-employee-app">GitHub Repo Link</a></p>
-          <p><a target="_blank" href="https://sharonkim09.github.io/react-employee-app/">Deployed Website Link</a></p>
+        <div className="col s12 m6">
+          <div className="card">
+            <div className="card-image">
+              <img alt="password generated" src="../password-generator.png" />
+              <span className="card-title">Password Generator</span>
+              <a
+                href="https://github.com/sharonkim09/gt-hw-js-password-generator"
+                className="btn-floating halfway-fab waves-effect waves-light red"
+              >
+                <i className="fa fa-github"></i>
+              </a>
+              <a
+                href="https://sharonkim09.github.io/gt-hw-js-password-generator"
+                className="btn-floating left halfway-fab waves-effect waves-light red"
+              >
+                <i className="fa fa-window-maximize"></i>
+              </a>
+            </div>
+            <div className="card-content">
+              <p>
+                I am a very simple card. I am good at containing small bits of
+                information. I am convenient because I require little markup to
+                use effectively.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
-    );
+      <div className="row">
+        <div className="col s12 m6">
+          <div className="card">
+            <div className="card-image">
+              <img alt="burger input" src="../burger-screenshot.png" />
+              <span className="card-title">Burger</span>
+              <a
+                href="https://github.com/sharonkim09/burger"
+                className="btn-floating halfway-fab waves-effect waves-light red"
+              >
+                <i className="fa fa-github"></i>
+              </a>
+              <a
+                href="https://damp-depths-84040.herokuapp.com/"
+                className="btn-floating left halfway-fab waves-effect waves-light red"
+              >
+                <i className="fa fa-window-maximize"></i>
+              </a>
+            </div>
+            <div className="card-content">
+              <p>
+                I am a very simple card. I am good at containing small bits of
+                information. I am convenient because I require little markup to
+                use effectively.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col s12 m6">
+          <div className="card">
+            <div className="card-image">
+              <img alt="signup/login" src="../donendusted.png" />
+              <span className="card-title">Done N Dusted</span>
+              <a
+                href="https://github.com/yoohooitstoo/grocr-receipt-app"
+                className="btn-floating halfway-fab waves-effect waves-light red"
+              >
+                <i className="fa fa-github"></i>
+              </a>
+              <a
+                href="https://fathomless-sands-26873.herokuapp.com/"
+                className="btn-floating left halfway-fab waves-effect waves-light red"
+              >
+                <i className="fa fa-window-maximize"></i>
+              </a>
+            </div>
+            <div className="card-content">
+              <p>
+                I am a very simple card. I am good at containing small bits of
+                information. I am convenient because I require little markup to
+                use effectively.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col s12 m6">
+          <div className="card">
+            <div className="card-image">
+              <img alt="notetaker app" src="../note-screenshot.png" />
+              <span className="card-title">Note Taker</span>
+              <a
+                href="https://github.com/sharonkim09/gt-hw-note-taker-2"
+                className="btn-floating halfway-fab waves-effect waves-light red"
+              >
+                <i className="fa fa-github"></i>
+              </a>
+              <a
+                href="https://peaceful-inlet-73696.herokuapp.com/"
+                className="btn-floating left halfway-fab waves-effect waves-light red"
+              >
+                <i className="fa fa-window-maximize"></i>
+              </a>
+            </div>
+            <div className="card-content">
+              <p>
+                I am a very simple card. I am good at containing small bits of
+                information. I am convenient because I require little markup to
+                use effectively.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col s12 m6">
+          <div className="card">
+            <div className="card-image">
+              <img alt="react employee directory" src="../employee-directory.png" />
+              <span className="card-title">Employee Directory</span>
+              <a
+                href="https://github.com/sharonkim09/react-employee-app"
+                className="btn-floating halfway-fab waves-effect waves-light red"
+              >
+                <i className="fa fa-github"></i>
+              </a>
+              <a
+                href="https://sharonkim09.github.io/react-employee-app/"
+                className="btn-floating left halfway-fab waves-effect waves-light red"
+              >
+                <i className="fa fa-window-maximize"></i>
+              </a>
+            </div>
+            <div className="card-content">
+              <p>
+                I am a very simple card. I am good at containing small bits of
+                information. I am convenient because I require little markup to
+                use effectively.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProjectSection;
